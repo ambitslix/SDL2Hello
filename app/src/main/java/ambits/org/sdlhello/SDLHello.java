9d1abe4d5e098b1188bd9a274b5139ffbf3f2d6f
Attila Mark <gougolith@gmail.com>
2021-09-03 14:04:37 -0700

    Initial issue with libmain.so not found

---
+++ app/src/main/java/ambits/org/sdlhello/SDLHello.java
@@ -0,0 +1,9 @@
+package ambits.org.sdlhello;
+
+import org.libsdl.app.SDLActivity;
+
+/**
+ * A sample wrapper class that just calls SDLActivity
+ */
+
+public class SDLHello extends SDLActivity { }
